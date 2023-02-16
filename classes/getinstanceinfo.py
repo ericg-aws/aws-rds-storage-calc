8ccf50086518c92b4fa977691a2487a7cfc5cc5c
Eric Garcia <grmeri@amazon.com>
2023-02-15 20:41:31 -0600

    fixing edge case string occurrence

--- classes/getinstanceinfo.py
+++ classes/getinstanceinfo.py
@@ -235,13 +235,13 @@ class Getinstanceinfo(object):
         temp_df = rds_pricing_df[rds_pricing_df['usageType'].str.contains(storage_iops)]
         per_unit = float(temp_df['PricePerUnit'].iat[0])
         # consider 3000 built in for baseline cost
-        iops_monthly_cost = float((storage_iops_adjusted) - 3000) * per_unit
+        iops_monthly_cost = float(int(storage_iops_adjusted) - 3000) * per_unit
 
         # calculate monthly storage Throughput costs 
         temp_df = rds_pricing_df[rds_pricing_df['usageType'].str.contains(storage_throughput)]
         per_unit = float(temp_df['PricePerUnit'].iat[0])
         # consider 125 MB/sec built in for baseline cost
-        throughput_monthly_cost = float((storage_throughput_adjusted) - 125) * per_unit
+        throughput_monthly_cost = float(int(storage_throughput_adjusted) - 125) * per_unit
 
         if args.percent_discount is not None:
             storage_cost = ((gb_monthly_cost + iops_monthly_cost + throughput_monthly_cost) * (1.0 - args.percent_discount))
